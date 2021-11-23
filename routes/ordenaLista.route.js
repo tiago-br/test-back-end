5df42e974f6033a9da515c4622e5117d8cccfd11
Tiago R <tiagobiagirebelato@gmail.com>
2021-11-23 12:46:08 -0300

    pasta routes criada

---
+++ routes/ordenaLista.route.js
@@ -0,0 +1,35 @@
+const {Router} = require('express')
+const route = Router()
+
+route.post("/ordenaLista", (request, response) => {
+    // recebe um payload no formato abaixo:
+    // {
+    // listas: {
+    //  salaN: [ 1, 5, 7, 8 ],
+    //  salaS: [ “a”, “x”, “n” ]
+    //  }
+    //}
+    const payload = request.body;
+    const { salaN, salaS } = payload.listas;
+    const copySalaN = [...salaN];
+    const copySalaS = [...salaS];
+  
+    //ordenando salaN em ordem numérica crescente
+  
+    const salaNordenada = copySalaN.sort((a, b) => a - b);
+  
+    //ordenando salaS em ordem alfabética
+  
+    const salaSordenada = copySalaS.sort((a, b) => a.localeCompare(b));
+  
+    //response
+    response.json({
+      listas: {
+        salaN: [...salaNordenada],
+        salaS: [...salaSordenada],
+      },
+    });
+  });
+
+  module.exports = route
+  
